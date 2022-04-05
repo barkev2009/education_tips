5ee9343bbeee55154a587f3ab5f08a5c96e546ea
Grigory <barkev2009@bk.ru>
2022-04-05 22:31:32 +0300

    added header

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,11 @@
 import React from 'react';
+import Header from './components/Header';
+import './styles/style.css'
 
 function App() {
   return (
     <div >
+      <Header />
       Clean slate
     </div>
   );
---
+++ src/components/Header.tsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <div className='header'>
+        <div className="header-item">
+            About
+        </div>
+        <div className="header-item">
+            Some text
+        </div>
+        <div className="header-item">
+            Some other text
+        </div>
+    </div>
+  )
+}
+
+export default Header
